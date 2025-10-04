245e0249c4a04c76499f58bb92806d3a3bd2ed99
Deepanshu Attri <iamattri0001@gmail.com>
2025-10-04 22:39:05 +0530

    print env load err

--- server/main.go
+++ server/main.go
@@ -10,7 +10,7 @@ import (
 func main() {
 	err := godotenv.Load()
 	if err != nil {
-		log.Print("Error loading .env file")
+		log.Print("Error loading .env file: ", err.Error())
 	}
 	app, err := bootstrap.NewApp()
 	if err != nil {
